0c5e8cd951caf81bddb3b10182e784ff7351c9e6
Rohan Dangal <johndangal2061@gmail.com>
2025-05-16 23:28:19 +0545

    basic objects datatypes and variables

---
+++ JavaScript/new.js
@@ -0,0 +1,19 @@
+console.log("hello world")
+//variables,data types and objects in javascript
+let a= "dangal"
+const b= true
+let c = undefined;
+let d= null
+//let is preferred over var because it is blocked space and var is global scope,
+console.log(a,b,c,d)
+console.log(typeof a,typeof b,typeof c,typeof d)
+alert("new virus detected")
+//js ma object vaneko chei funtion banayeko jastai ho
+let y =
+{
+    name : "rohan",
+    salary : "2 crore",
+    "is_handsome":true
+}
+console.log(y)
+y.address="lalitpur"
