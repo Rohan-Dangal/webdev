7a358ad3a45aa9c7a0bbc6d596e78f4f4cec1742
Rohan Dangal <johndangal2061@gmail.com>
2025-05-17 17:14:01 +0545

    basics of js

---
+++ JavaScript/56_conditionals.js
@@ -0,0 +1,18 @@
+console.log("new daynew energy")
+let age = 23;
+if(age>=18)
+{
+    console.log("you are eligible for voting");
+}
+else{
+    console.log("you are not eligible for voting");
+}
+let a = 3;
+let b = "3";
+console.log(a===b); //=== checks value and type of whereas == only checks the value.
+
+
+//ternary operator
+let r = 7;
+let o = 9;
+let h = r>d?(r-d):(d-r);
