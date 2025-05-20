4a87472bc84bbee49fbff3d9349f08a3952ddfe9
Rohan Dangal <johndangal2061@gmail.com>
2025-05-20 21:48:21 +0545

    strings in js

---
+++ JavaScript/60_string.js
@@ -0,0 +1,10 @@
+let real_name = "rohan";
+let friend = "harry";
+console.log("his name is"+real_name+" and his friend is "+friend);
+console.log(`his name is ${real_name}and his friend name is ${friend} `)//this is called template literals.
+let a = "rohan";
+let b = "krishna";
+console.log(b.slice(2,6));//it will print the string from 2nd index to 6th index(6th one will not be included)
+console.log(b.replace("h", "god"));//it will replace the first occurrence of h with god
+//concatenation
+console.log(a.concat(" is the son of ", b) );
