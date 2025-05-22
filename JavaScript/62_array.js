836a6d86e7e38c89e4f4cbc72c36c0c523301ad1
Rohan Dangal <johndangal2061@gmail.com>
2025-05-22 14:13:38 +0545

    concepts of arrays in js

---
+++ JavaScript/62_array.js
@@ -0,0 +1,62 @@
+// array is defined as the collection of multiple data types
+//array are mutable while strings are immutable.
+// let arr = [1,3,5,7];
+// console.log(arr.toString());//it converts array into string
+// console.log(arr.join(" and ")); //joins the elements
+
+//concatination
+// let arr1 = [1,4,8]
+// let arr2 = [2,5,7]
+// let arr3 = [2,7,8]
+// console.log(arr1.concat(arr2,arr3)); //it concatenates the arrays
+
+// let a = [23, 45, 67, 89, 12, 34, 56, 78, 90];
+
+// a.forEach(function(value, index, arr) {            //for each loop
+//     console.log(value, index, arr);
+// });
+
+
+//for in loop
+
+// let john =
+// {
+//     real_name: "rohan dangal",
+//     age: 20
+// };
+// for (const key in john) {
+//     if (Object.prototype.hasOwnProperty.call(john, key)) {
+//         const element = john[key];
+//         console.log(key, element);
+        
+//     }
+// }
+
+
+// concept of map:
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let newarr = arr.map(e=>{
+//     return e**2;
+// });
+// console.log(newarr);
+
+
+
+//concept of filter:
+// let arr = [1, 2, 23, 4, 55];
+
+// const greaterthanseven = (e => {
+//     if (e > 7) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
+
+// let filteredArr = arr.filter(greaterthanseven);
+// console.log(filteredArr);
+
+
+ 
