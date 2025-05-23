f1572b52ad8bcd8d8f10d7926b7a13f74dc658f4
Rohan Dangal <johndangal2061@gmail.com>
2025-05-23 17:08:26 +0545

    basic concepts of document object model

---
+++ JavaScript/66_document_obj_model.js
@@ -0,0 +1,10 @@
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
+// const first = document.body.firstElementChild
+// undefined
+// first.lastElementChild
+// <div class=​"box">​box5​</div>​
+// first.lastElementChild.style.color = "red";
+// 'red'
+// first.firstElementChild.style.backgroundColor = "yellow";
+// 'yellow'
--- JavaScript/ex_11_factorial.js
+++ JavaScript/ex_11_factorial.js
@@ -1,31 +1,5 @@
 //program to generate factorial of a number
-// const readline = require(`readline`);
-// const rl = readline.createInterface({
-//     input:process.stdin,
-//     output:process.stdout
-// });
-// rl.question("enter the number of which you want the factorial of:",function(input)
-// {
-//     const number = Number(input);
-// })
-// function factorial(n)
-// {
-//     if(n<0)
-//     {
-//         console.log("invalid choice")
-//         return 0;
-//     }
-//     else if(n==0 || n==1)
-//     {
-//     }
-//     else
-//     {
-//         return n * factorial(n-1);
-//     }
-//     const result = factorial(number);
-//     console.log("factorial is:"+ result)
-//     rl.close();
-// }
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
