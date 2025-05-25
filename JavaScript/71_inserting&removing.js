ecd30e24198cc1213be3e8a0f06b744abe0e1c45
Rohan Dangal <johndangal2061@gmail.com>
2025-05-25 21:32:52 +0545

    inserting and removing in dom

---
+++ JavaScript/71_inserting&removing.js
@@ -0,0 +1,5 @@
+document.querySelector(".box").innerHTML //we can get the inner html which is the text inside the box.
+document.quetyselector(".container").outerHTML //we can get the outer html which is the text inside the container and the box.
+document.quetyselector(".container").textContent //we can get the text content which is the text inside the container and the box without the html tags.
+document.querySelector(".container").addclass("new-class") //we can add a new class to the container.
+document.querySelector(".container").removeClass("new-class") //we can remove a class from the container.
