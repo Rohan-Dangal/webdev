c2e372acef8551be95fcb4c2bb2d903d21f7d762
Rohan Dangal <johndangal2061@gmail.com>
2025-06-14 19:53:56 +0545

    pq from functions and loops

---
+++ CW PQ C-3(loops and function)/question1.js
@@ -0,0 +1,12 @@
+//wap to print the marks of an student in an obj using for loop.
+let student = 
+{
+    Ram : {},
+    hari : {}
+};
+let subject = ["html","css"];
+
+for(let i = 0;i < 2;i++)
+{
+    console.log("marks of ram is${aubject                                                                           }:")
+}
